e6a9f8f18642ebc343a13a827957e37f7044b67c
AT-OY <at.ouyang0102@gmail.com>
2024-03-19 17:22:55 -0400

    2024/3/19 Finish Course 3

---
+++ personal_practice/test_day1.c
@@ -0,0 +1,38 @@
+//// 002
+//#include <stdio.h>
+//
+//int main()
+//{
+//    printf("Hello, World!\n");
+//
+//    return 0;
+//}
+
+
+
+//// 003
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("%zu\n", sizeof(char));
+//	printf("%zu\n", sizeof(short));
+//	printf("%zu\n", sizeof(int));
+//	printf("%zu\n", sizeof(long));
+//	printf("%zu\n", sizeof(long long));
+//	printf("%zu\n", sizeof(float));
+//	printf("%zu\n", sizeof(double));
+//
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int age = 20;
+//	float price = 66.6;
+//	
+//	return 0;
+//}

bf9c4f3732f01132313a6808e1617b2d4aedf4a0
AT-OY <at.ouyang0102@gmail.com>
2024-03-21 16:44:10 -0400

    2024/3/21 Finish Course 10

--- personal_practice/test_day2.c
+++ personal_practice/test_day2.c
@@ -38,26 +38,82 @@
 //}
 
 
-// 4. 枚举常量
-#include <stdio.h>
+//// 4. 枚举常量
+//#include <stdio.h>
+//
+//enum Color
+//{
+//	// 枚举常量
+//	RED,
+//	GREEN,
+//	BLUE
+//};
+//
+//int main()
+//{
+//	// RGB
+//	enum Color c = RED;
+//	enum Color d = GREEN;
+//	enum Color e = BLUE;
+//	printf("%d\n", c);
+//	printf("%d\n", d);
+//	printf("%d\n", e);
+//
+//	return 0;
+//}
 
-enum Color
-{
-	// 枚举常量
-	RED,
-	GREEN,
-	BLUE
-};
 
-int main()
-{
-	// RGB
-	enum Color c = RED;
-	enum Color d = GREEN;
-	enum Color e = BLUE;
-	printf("%d\n", c);
-	printf("%d\n", d);
-	printf("%d\n", e);
+//#include <stdio.h>
+//#include <string.h>
+//
+//int main()
+//{
+//	char ch = 'w';
+//	char arr1[] = "abcdef";
+//	char arr2[] = {'a', 'b', 'c', 'd', 'e', 'f'};
+//	char arr3[] = {'a', 'b', 'c', 'd', 'e', 'f', '\0'};
+//
+//	printf("arr1: %s\n", arr1);
+//	printf("arr2: %s\n", arr2);
+//	printf("arr3: %s\n", arr3);
+//	printf("Len of arr1: %zd\n", strlen(arr1));  // 长度不包含 \0，看 \0 之前出现多少个字符
+//	printf("Len of arr2: %zd\n", strlen(arr2));
+//
+//	return 0;
+//}
 
-	return 0;
-}
+
+//// 转义字符
+//#include <stdio.h>
+//
+//int main()
+//{
+//	// 三字母词，早期设备 ??) --> ]
+//	printf("%s\n", "(Are you ok\?\?)");
+//	printf("%c\n", '\'');
+//	printf("abc\"def\n");
+//	printf("abc\\0def\n");
+//	printf("abc\adef\n");
+//	printf("abc\tdef\n");
+//	printf("a: \130 \n");
+//	printf("a: \628 \n");
+//	printf("c: \x7a \n");
+//
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	/*
+//	int a = 10;
+//	int b = 20;
+//	*/
+//
+//	// 创建指针变量p，并赋值为NULL
+//	int* p = NULL;
+//
+//	return 0;
+//}

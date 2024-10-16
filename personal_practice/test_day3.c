cd9395ef03be1db715223b5617d333a125cb65c4
AT-OY <at.ouyang0102@gmail.com>
2024-10-16 18:11:31 -0400

    2024/10/16 Finish Course 20

---
+++ personal_practice/test_day3.c
@@ -0,0 +1,101 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+
+//// 017
+//int main() {
+//	int a = 7 / 2;
+//	int b = 7 % 2;
+//	float c = 7 / 2.0;
+//
+//	printf("%d\n", a);
+//	printf("%d\n", b);
+//	printf("%.1f\n", c);
+//
+//	return 0;
+//}
+
+
+//int main() {
+//	int a = 0;  // 初始化
+//	a = 20;  // 赋值
+//	a += 3;
+//	printf("%d\n", a);
+//
+//	return 0;
+//}
+
+
+//int main() {
+//	int flag = 0;
+//	if (!flag) {
+//		printf("HeHe\n");
+//	}
+//
+//	return 0;
+//}
+
+
+//int main() {
+//	int a = 10;
+//	int arr[10] = { 0 };
+//	printf("%zd\n", sizeof(a));
+//	printf("%zd\n", sizeof(int));
+//	printf("%zd\n", sizeof a);
+//	printf("%zd\n", sizeof arr);
+//
+//	return 0;
+//}
+
+
+//int main() {
+//	int a = 10;
+//	int b = a++;
+//	int c = ++a;
+//	printf("%d\n", a);
+//	printf("%d\n", b);
+//	printf("%d\n", c);
+//
+//	return 0;
+//}
+
+
+//int main() {
+//	int a = (int)3.14;  // 字面浮点数编译器默认 double
+//	printf("%d\n", a);
+//
+//	return 0;
+//}
+
+
+//int main() {
+//	int a = 10;
+//	int b = 20;
+//	if (a && b) {
+//		printf("HeHe\n");
+//	}
+//
+//	return 0;
+//}
+
+
+//int main() {
+//	int a = 10;
+//	int b = 20;
+//	int r = (a > b ? a : b);
+//	printf("%d\n", r);
+//
+//	return 0;
+//}
+
+
+//int main() {
+//	int a = 10;
+//	int b = 20;
+//	int c = 0;
+//	int d = (c = a - 2, a = b + c, c - 3);
+//	printf("%d %d %d %d\n", a, b, c, d);
+//
+//	return 0;
+//}
---
+++ personal_practice/test_day4.c
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+
+//// 020
+//typedef unsigned int uint;
+//
+//typedef struct Node {
+//	int data;
+//	struct Node* next;
+//}Node;
+//
+//int main() {
+//	unsigned int num0 = 0;
+//	uint num2 = 1;
+//	struct Node n;
+//	Node n2;
+//
+//	return 0;
+//}
+
+
+//void test() {
+//	static int a = 1;
+//	a++;
+//	printf("%d ", a);
+//}
+//
+//int main() {
+//	int i = 0;
+//	while (i < 10) {
+//		test();
+//		printf("%d\n", i++);
+//	}
+//	return 0;
+//}
+
+
